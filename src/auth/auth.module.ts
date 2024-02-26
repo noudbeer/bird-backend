fd362da11373049013b40eefd7408dbe4207a5dd
Simon Bernoud <simon.bernoud@ikmail.com>
2024-02-26 20:45:27 +0100

    add jwt token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
-  controllers: [AuthController],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s'},
+    })
+  ],
   providers: [AuthService],
-  imports: [UsersModule],
+  controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
---
+++ src/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+    secret: 'DO NOT USE THIS VALUE. This value is secret to jwt: $9$ELD&@9aYRS659XjYy$?Jh&@&#RT?hEQG7X?LG.'
+}
